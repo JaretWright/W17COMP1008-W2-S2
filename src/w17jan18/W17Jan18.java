12112c726d843470f4f0d2685db19b5448256f8b
JWright <JWright@192.168.56.1>
2017-01-18 12:23:50 -0500

    Initial Submission

---
+++ src/w17jan18/W17Jan18.java
@@ -0,0 +1,17 @@
+
+package w17jan18;
+
+/**
+ *
+ * @author JWright
+ */
+public class W17Jan18 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub world!!");
+    }
+    
+}
